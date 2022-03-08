a3441556b5d60b7fa4f57add8a18e909cbc5abd8
SaiYugandharRajendran <100206803+SaiYugandharRajendran@users.noreply.github.com>
2022-03-08 13:10:03 +0530

    Delete EStockMgmtApp directory

--- EStockMgmtApp/src/main/java/com/example/demo/exception/CompanyCodeAlreadyExistsException.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.exception;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(code=HttpStatus.CONFLICT,reason="Company code already exists!")
-public class CompanyCodeAlreadyExistsException extends Exception{
-
-}
